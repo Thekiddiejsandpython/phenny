755059efe317d13b0ccc7e97b862098a1344f62d
Thekiddiejsandpython <bustida@gmail.com>
2017-12-09 23:23:24 -0700

    Create hangman.py

--- modules/hangman.py
+++ modules/hangman.py
@@ -1,1 +1,36 @@
-python stuff
+import random
+words = ["begiak", "apertium", "dictionary", "processing", "language", "natural", "phenny", "lexical", "analysis", "generator"]
+
+# Phenny.say("Guess")
+def game(phenny, input):
+  word = words[random.randint(0, len(words)-1)]
+  msg = "-" * len(word)
+  phenny.say("Type: 'quit' to stop playing")
+  while (msg != word):
+    guess = input("What is your Guess: ")
+    if(len(guess) > 1):
+      if(guess=="quit"):
+        msg=word
+      elif guess == word:
+        msg=word
+      else:
+        phenny.say("One letter at a time (Unless you write the correct answer)")
+    if word.find(guess) != (-1) :
+      tmp = list(msg)
+      for a in range(0, len(tmp)):
+        if(guess == word[a]):
+          tmp[a] = word[a]
+      msg = ''.join(tmp)
+      phenny.say(msg)
+    else:
+      phenny("try again")
+      phenny(msg)
+  
+   phenny.say("Begiak is proud :D")    
+
+hangman.commands = ['hangman'] 
+hangman.name = 'hangman'
+hangman.example = '.hangman'
+
+if __name__ == '__main__':
+print(__doc__.strip())
